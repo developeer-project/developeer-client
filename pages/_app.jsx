0b4bb4ddac6bbd34a30efba874bc1f0ec2e7beff
Maulik Pandya <pandyamaulik2001@gmail.com>
2022-02-26 13:59:10 +0530

    singup github not working

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,46 +1,11 @@
-import Head from "next/head";
-import { MantineProvider, NormalizeCSS, GlobalStyles } from "@mantine/core";
-import { NotificationsProvider } from "@mantine/notifications";
-import { useState } from "react";
-import { ColorSchemeProvider } from "@mantine/core";
+import { Provider } from "next-auth/client";
 
-import "../styles/globals.scss";
-import Layout from "../components/Layout";
-
-export default function App(props) {
-  const { Component, pageProps } = props;
-  const [colorScheme, setColorScheme] = useState("light");
-
-  const toggleColorScheme = (value) =>
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-
+const App = ({ Component, pageProps }) => {
   return (
-    <>
-      <Head>
-        <title>Welcome to Developeer</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <ColorSchemeProvider
-        colorScheme={colorScheme}
-        toggleColorScheme={toggleColorScheme}>
-        <MantineProvider
-          theme={{
-            /** Put your mantine theme override here */
-            colorScheme,
-            loader: "bars",
-          }}>
-          <NormalizeCSS />
-          <GlobalStyles />
-          <NotificationsProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </NotificationsProvider>
-        </MantineProvider>
-      </ColorSchemeProvider>
-    </>
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
   );
-}
+};
+
+export default App;
