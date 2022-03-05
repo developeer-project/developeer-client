7a8c0df3bcb97a6852894d1b62ab8f4c03aa1a5b
gunjankayarkar <gkkayarkar@gmail.com>
2022-03-05 19:55:02 +0530

    improving footer referring figma

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,11 +5,24 @@ import styles from "../styles/Navbar.module.scss";
 const Footer = () => {
   return (
     <div className={styles.foot_wrap}>
+
+      <div className={styles.foot_left_wrap}>
+      <img  src="https://cdn.discordapp.com/attachments/746292114429837313/948179323725181030/Letter_D_GIF.gif" alt="logo" />
+      </div>
+
+      <div className={styles.foot_right_wrap}>
+
+      
       <div className={styles.foot_menu_wrap}>
-      <div class="d1">About us</div>
-  <div class="d2">Contact us</div>
-  <div class="d3">Terms & Conditions</div>
-  <div class="d4">Pickachu</div>
+      <div className="d1">Home</div>
+  <div className="d2">Contact us</div>
+  <div className="d3">Disclaimer</div>
+  <div className="d4">Terms & Conditions</div>
+  <div className="d5">Report Bugs</div>
+         </div>
+
+
+
       </div>
     </div>
   );
