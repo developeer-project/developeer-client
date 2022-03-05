0925ad7309ece56232e7f84b2ccf3bfbb39557e5
gunjankayarkar <gkkayarkar@gmail.com>
2022-03-06 01:26:45 +0530

    Header and footer completed. Responsiveness pending. Center main content pending. Brigh border colors added for easy understanding of code, to be removed later on

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,24 +5,64 @@ import styles from "../styles/Navbar.module.scss";
 const Footer = () => {
   return (
     <div className={styles.foot_wrap}>
-
       <div className={styles.foot_left_wrap}>
-      <img  src="https://cdn.discordapp.com/attachments/746292114429837313/948179323725181030/Letter_D_GIF.gif" alt="logo" />
+        <img
+          src="https://cdn.discordapp.com/attachments/746292114429837313/948179323725181030/Letter_D_GIF.gif"
+          alt="logo"
+        />
+        <div className={styles.text_below_logo}>
+          © 2022 Registered Trademark
+        </div>
       </div>
 
       <div className={styles.foot_right_wrap}>
-
-      
-      <div className={styles.foot_menu_wrap}>
-      <div className="d1">Home</div>
-  <div className="d2">Contact us</div>
-  <div className="d3">Disclaimer</div>
-  <div className="d4">Terms & Conditions</div>
-  <div className="d5">Report Bugs</div>
-         </div>
-
-
-
+        <div className={styles.foot_menu_wrap}>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>Home</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>Contact</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>Disclaimer</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>T and C</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>Help</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
