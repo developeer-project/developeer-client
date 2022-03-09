f11226453d2c715fcefc913134db41e4a2691f13
Aryaman Purkait <aryaman.purkait@gmail.com>
2022-03-10 01:46:28 +0530

    Q1

---
+++ components/Quote.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card, Image, Text, Badge, Button, Group, useMantineTheme } from '@mantine/core';
+
+const Quote = () => {
+    return (
+        <div>
+           <h1>BLEHHHH</h1>
+        </div>
+       );
+}
