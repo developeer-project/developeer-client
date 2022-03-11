2ec3a5832020e4b756f70d1858be470a625a8b50
gunjankayarkar <gkkayarkar@gmail.com>
2022-03-12 02:54:09 +0530

    footer added; header footer both responsiveness and final changes pending

--- components/homepage/BottomSect.jsx
+++ components/homepage/BottomSect.jsx
@@ -1,9 +1,71 @@
+import React from "react";
+import styles from "../../styles/homepage-comps/bottomsect.module.scss";
+
 const BottomSect = () => {
     return (
-        <section>
-            bottom section
-        </section>
-    );
-}
+        <div className={styles.foot_wrap}>
+      <div className={styles.foot_left_wrap}>
+        <img
+          src="https://cdn.discordapp.com/attachments/746292114429837313/948179323725181030/Letter_D_GIF.gif"
+          alt="logo"
+        />
+        <div className={styles.text_below_logo}>
+          © 2022 Registered Trademark
+        </div>
+      </div>
+
+      <div className={styles.foot_right_wrap}>
+        <div className={styles.foot_menu_wrap}>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>Home</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>Contact</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>Disclaimer</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>T and C</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+          <div className="d1">
+            <ul>
+              <li>
+                <b>Help</b>
+              </li>
+              <li>Find</li>
+              <li>Messages</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 export default BottomSect;
--- pages/home.jsx
+++ pages/home.jsx
@@ -5,24 +5,24 @@ import BottomSect from "../components/homepage/BottomSect";
 
 import Head from "next/head";
 
-import { Parallax, ParallaxLayer } from "@reactspring/parallax";
+//import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 const pagetitle = () => "Develo<peer/>";
 
 const HomePage = () => {
-  useEffect(() => {
-    const threeScript = document.createElement("script");
-    threeScript.setAttribute("id", "threeScript");
-    threeScript.setAttribute(
-      "src",
-      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
-    );
-    document.getElementsByTagName("head")[0].appendChild(threeScript);
-    return () => {
-      if (threeScript) {
-        threeScript.remove();
-      }
-    };
-  }, []);
+  // useEffect(() => {
+  //   const threeScript = document.createElement("script");
+  //   threeScript.setAttribute("id", "threeScript");
+  //   threeScript.setAttribute(
+  //     "src",
+  //     "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+  //   );
+  //   document.getElementsByTagName("head")[0].appendChild(threeScript);
+  //   return () => {
+  //     if (threeScript) {
+  //       threeScript.remove();
+  //     }
+  //   };
+  // }, []);
 
   return (
     <>
