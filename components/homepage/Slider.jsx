29445e3bb053a62934bde887e23aa44c7b16c23f
Maulik Pandya <pandyamaulik2001@gmail.com>
2022-03-13 14:48:23 +0530

    nav, slidercard scroll

--- components/homepage/Slider.jsx
+++ components/homepage/Slider.jsx
@@ -3,11 +3,16 @@ import React from "react";
 import styles from "../../styles/homepage-comps/slider.module.scss";
 import SliderCard from "./SliderCard";
 
+
 const Slider = () => {
   return (
     <div className={styles.root_box}>
       {/* <h3>Slider box</h3> */}
       {/* slider box */}
+
+      <marquee behaviour="scroll" direction="down" scrollamount='2' behavior="scroll" scrolldelay
+="85" height="90%" >
+
       <SliderCard />
       <SliderCard />
       <SliderCard />
@@ -18,6 +23,9 @@ const Slider = () => {
       <SliderCard />
       <SliderCard />
       <SliderCard />
+     </marquee>
+
+
     </div>
   );
 };
