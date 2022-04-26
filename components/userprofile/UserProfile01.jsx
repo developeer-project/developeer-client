0192994eff2436795079eed711d92d727d30c5c9
Aryaman Purkait <aryaman.purkait@gmail.com>
2022-04-26 10:13:16 +0530

    proposed right section (incomplete)

--- components/userprofile/UserProfile01.jsx
+++ components/userprofile/UserProfile01.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "../../styles/userprofilecss/userprofile01.module.scss";
 
+
 const userprofile01 = () => {
   return (
     <div className={styles.profile_wrap}>
@@ -24,8 +25,80 @@ const userprofile01 = () => {
       </div>
 
       <div className={styles.profile_right_wrap}>
-        aur kya details daalne hai idhar that goes here
+        aur kya details daalne hai idhar that goes here 
+        <div className={styles.header}>
+          <h2>About</h2>
+        </div>
+        Baby shark, doo doo doo doo doo doo
+Baby shark, doo doo doo doo doo doo
+Baby shark, doo doo doo doo doo doo
+Baby shark!
 
+Mommy shark, doo doo doo doo doo doo
+Mommy shark, doo doo doo doo doo doo
+Mommy shark, doo doo doo doo doo doo
+Mommy shark!
+
+Daddy shark, doo doo doo doo doo doo
+Daddy shark, doo doo doo doo doo doo
+Daddy shark, doo doo doo doo doo doo
+Daddy shark!
+
+Grandma shark, doo doo doo doo doo doo
+Grandma shark, doo doo doo doo doo doo
+Grandma shark, doo doo doo doo doo doo
+Grandma shark!
+
+Grandpa shark, doo doo doo doo doo doo
+Grandpa shark, doo doo doo doo doo doo
+Grandpa shark, doo doo doo doo doo doo
+Grandpa shark!
+
+Let’s go hunt, doo doo doo doo doo doo
+Let’s go hunt, doo doo doo doo doo doo
+Let’s go hunt, doo doo doo doo doo doo
+Let’s go hunt!
+    {/* <div className={styles.profile_left_wrap}>
+      Left Section
+    </div> */}
+    <div className={styles.header}>
+          <h2>Skills</h2>
+        </div>
+    <div className={styles.profile_card}>
+    <div className={styles.row}>
+  <div className={styles.column}>
+    <div className={styles.card}>
+      <h3>Card 1</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+
+  <div className={styles.column}>
+    <div className={styles.card}>
+      <h3>Card 2</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+  
+  <div className={styles.column}>
+    <div className={styles.card}>
+      <h3>Card 3</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+  
+  <div className={styles.column}>
+    <div className={styles.card}>
+      <h3>Card 4</h3>
+      <p>Some text</p>
+      <p>Some text</p>
+    </div>
+  </div>
+</div>
+</div>
       </div>
     </div>
   );
